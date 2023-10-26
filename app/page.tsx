6a741d6233af996773ce1758455fdec2e3722f35
Luispe <38361000+lpbonomi@users.noreply.github.com>
2023-10-26 20:34:22 -0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES, WALLET_ADAPTERS, IProvider } from "@web3auth/base";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import { SolanaPrivateKeyProvider } from "@web3auth/solana-provider";
+import { useEffect, useState } from "react";
+
+const Web3authButtons = () => {
+  const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
+  const [provider, setProvider] = useState<IProvider | null>(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3authInstance = new Web3Auth({
+          clientId: "your_client_id",
+          web3AuthNetwork: "sapphire_devnet",
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.SOLANA,
+            chainId: "0x3",
+            rpcTarget: "https://api.devnet.solana.com",
+          },
+          sessionTime: 86400,
+          enableLogging: false,
+          uiConfig: {
+            defaultLanguage: "es",
+          },
+        });
+
+        const privateKeyProvider = new SolanaPrivateKeyProvider({
+          config: {
+            chainConfig: {
+              chainId: "0x3",
+              rpcTarget: "https://api.devnet.solana.com",
+              displayName: "Solana",
+              blockExplorer: "https://explorer.solana.com",
+              ticker: "SOL",
+              tickerName: "Solana",
+            },
+          },
+        });
+
+        const openloginAdapter = new OpenloginAdapter({
+          privateKeyProvider,
+          adapterSettings: {
+            uxMode: "redirect",
+          },
+        });
+
+        web3authInstance.configureAdapter(openloginAdapter);
+
+        setWeb3auth(web3authInstance);
+
+        await web3authInstance.initModal();
+        setProvider(web3authInstance.provider);
+
+        if (web3authInstance.connectedAdapterName) {
+          setLoggedIn(true);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  async function login() {
+    if (!web3auth) {
+      console.log("web3auth not initialized");
+      return;
+    }
+    await web3auth.connect();
+    await web3auth.authenticateUser();
+  }
+
+  async function getPrivateKey() {
+    if (!provider) {
+      console.log("provider not initialized");
+      return;
+    }
+
+    return await provider.request({
+      method: "solanaPrivateKey",
+    });
+  }
+
+  async function handleLogin() {
+    await login();
+  }
+
+  async function handleGetPrivateKey() {
+    await getPrivateKey();
+  }
+
+  return (
+    <div>
+      <button onClick={handleLogin} className="p-5">
+        Connect
+      </button>
+      <button onClick={handleGetPrivateKey}>Get Private Key</button>
+    </div>
+  );
+};
+
+export default function MyApp() {
+  return <Web3authButtons />;
+}
